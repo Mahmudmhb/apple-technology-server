199ae9c903da9cbceb982109eda4eb2ebed10f36
mahmudmhb1 <mahmud.mhb1@gmail.com>
2024-02-15 16:18:24 +0600

    mongodb cors express added

---
+++ index.js
@@ -0,0 +1,77 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const port = process.env.PORT || 5000;
+app.use(cors())
+app.use(express.json())
+
+// kU07FWJdJ2dPGPLy
+// technoloy-phone
+
+
+
+
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const uri = "mongodb+srv://technoloy-phone:kU07FWJdJ2dPGPLy@cluster0.gegfn.mongodb.net/?retryWrites=true&w=majority";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    const categoryCollection = client.db("AppleDB").collection('category')
+    const productsCollection = client.db('AppleDB').collection('addProducts')
+
+app.get('/products', async(req, res)=>{
+    const cursor = await productsCollection.find().toArray()
+    res.send(cursor)
+
+})
+app.get('/products/:id', async(req, res)=>{
+  const id = req.params.id
+  console.log(id)
+  const query = { _id: new ObjectId(id)}
+   const product = await productsCollection.findOne(query)
+   console.log(product)
+   res.send(product)
+})
+    app.post('/products', async(req, res)=>{
+        const products = req.body;
+        console.log(products)
+        const result = await productsCollection.insertOne(products)
+        res.send(result)
+    })
+
+app.post('/category', async(req, res)=>{
+    const category = req.body
+    console.log(category)
+})
+
+
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+app.get('/', (req, res)=>{
+    console.log('hittng the server');
+    res.send('this is server site')
+})
+
+app.listen(port, ()=>{
+    console.log(`this is listening port of ${port}`)
+})
